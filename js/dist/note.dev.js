e12957364ea2e928339475dd22904fb4941910d1
1211428762 <1211428762@qq.com>
2020-09-14 09:43:53 +0800

    网页化

---
+++ js/dist/note.dev.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$(".eyeBtn").click(function () {
+  $("body").toggleClass("eyeCare");
+  $("main").toggleClass("eyeCare");
+  $(".notice").toggleClass("eyeCare");
+  $(".backTop").toggleClass("eyeCare");
+  $(".eyeBtn").text() == "切换暗色" ? $(".eyeBtn").text("切换护眼色") : $(".eyeBtn").text("切换暗色");
+});
+$(".jqBtn").click(function () {
+  $(".jqBtn").text() == "不看Jquery" ? $(".jqBtn").text("还是看吧") : $(".jqBtn").text("不看Jquery");
+  $(".jquery").slideToggle("slow");
+});
+$(".bsBtn").click(function () {
+  $(".bsBtn").text() == "不看bootstrap" ? $(".bsBtn").text("还是看吧") : $(".bsBtn").text("不看bootstrap");
+  $(".bootstrap").slideToggle("slow");
+});
+$(".backTop").click(function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
